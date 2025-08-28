9e625aa9ee5e73d4cbceb5686876204668dde5d9
arminxu <arminxu@tencent.com>
2025-08-28 18:52:24 +0800

    continue

---
+++ leetcode/56.合并区间.py
@@ -0,0 +1,50 @@
+"""
+56. 合并区间
+以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+
+
+示例 1：
+输入：intervals = [
+[1,3],
+[2,6],
+[8,10],
+[15,18]
+]
+输出：[[1,6],[8,10],[15,18]]
+解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+
+示例 2：
+输入：intervals = [[1,4],[4,5]]
+输出：[[1,5]]
+解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+"""
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        for i in range(len(intervals)):
+            for j in range(i, len(intervals)):
+                if intervals[i][0] > intervals[j][0]:
+                    temp = intervals[i]
+                    intervals[i] = intervals[j]
+                    intervals[j] = temp
+        res = [intervals[0]]
+        for interval in intervals[1:]:
+            last_end = res[-1][1]
+            if last_end >= interval[0]:  # 合并
+                res[-1] = [res[-1][0], max(last_end, interval[1])]
+            else:
+                res.append(interval)
+        return res
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.merge([
+        [1, 3],
+        [2, 6],
+        [8, 10],
+        [15, 18]
+    ]))
